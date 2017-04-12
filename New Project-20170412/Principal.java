24a8fe0a17aa6244b13adea6ba9a6ecf86971967
marioreis86 <mariopimenta86@gmail.com>
2017-04-12 10:04:21 -0300

    Created by Coding Ground Client

---
+++ New Project-20170412/Principal.java
@@ -0,0 +1,8 @@
+public class Principal
+{
+    public static void main(String[] args)
+    {
+        Object vendedor = new Vendedor("28871", new Equipe(), "Mario", "marioreis86", "12224422725");
+        System.out.println(vendedor);
+    }
+}
